21314c4c06bd4219e871f4e1c0d74dec0d1bc20c
sagar <snkumar.nayak@gmail.com>
2017-10-26 11:30:16 +0530

    Description

--- app/src/main/java/com/sagar/android_projects/coordinatorlayoutbehaviour/NormalAppbarLayout.java
+++ app/src/main/java/com/sagar/android_projects/coordinatorlayoutbehaviour/NormalAppbarLayout.java
@@ -8,8 +8,15 @@ import android.support.v7.widget.Toolbar;
 
 import com.sagar.android_projects.coordinatorlayoutbehaviour.adapter.Adapter;
 
+/**
+ * created by SAGAR KUMAR NAYAK on 26 OCT 2017.
+ * this is an activity to demonstrate the use of scroll flag in appbar layout.
+ * this will use a enter always scroll flag. due to which the toolbar will auto hide on the down scroll
+ * and show up in every up scroll.
+ */
 public class NormalAppbarLayout extends AppCompatActivity {
 
+    //views
     RecyclerView recyclerView;
 
     @Override
@@ -19,8 +26,14 @@ public class NormalAppbarLayout extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        //view binding
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview_normal_appbar_layout);
+        ////////////////////////////////////////////////////////////////////////////////////////////
 
+        /*
+        set up layout manager and adapter for recyclerview.
+         */
         recyclerView.setLayoutManager(new LinearLayoutManager(NormalAppbarLayout.this));
         recyclerView.setAdapter(new Adapter());
     }
