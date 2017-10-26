e1d58dcaf7a483961ecf08ae427be9a9f6a4d48d
sagar <snkumar.nayak@gmail.com>
2017-10-26 11:42:37 +0530

    Description

--- app/src/main/java/com/sagar/android_projects/coordinatorlayoutbehaviour/CollapsingToolbarLayout.java
+++ app/src/main/java/com/sagar/android_projects/coordinatorlayoutbehaviour/CollapsingToolbarLayout.java
@@ -10,8 +10,19 @@ import android.support.v7.widget.Toolbar;
 
 import com.sagar.android_projects.coordinatorlayoutbehaviour.adapter.Adapter;
 
+/**
+ * created by SAGAR KUMAR NAYAK on 26 OCT 2017.
+ * this is an activity to demonstrate the use of collapsingtoolbar inside the appbar layout.
+ * it will have a larger appbar at the start. but on down scroll the appbar will shrink to a small
+ * size to make more list items visible to user.
+ * it uses a scroll flag exit until collapsed, which results in shrink of appbar to a min height for
+ * a down scroll. and at the end of the up scroll it will come to its normal expanded state.
+ * this class also allows you to show a required length of appbar with showing the full length of the
+ * expanded appbar. this is achieved by the function setAppBarOffset().
+ */
 public class CollapsingToolbarLayout extends AppCompatActivity {
 
+    //views
     RecyclerView recyclerView;
     AppBarLayout appBarLayout;
     CoordinatorLayout coordinatorLayout;
@@ -23,14 +34,23 @@ public class CollapsingToolbarLayout extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        //view binding
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview_collapsing_toolbar);
+        appBarLayout = (AppBarLayout) findViewById(R.id.appbarlayout);
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorlayout);
+        ////////////////////////////////////////////////////////////////////////////////////////////
 
+        /*
+        set layout manager and adapter to recyclerview
+         */
         recyclerView.setLayoutManager(new LinearLayoutManager(CollapsingToolbarLayout.this));
         recyclerView.setAdapter(new Adapter());
 
-        appBarLayout = (AppBarLayout) findViewById(R.id.appbarlayout);
-        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorlayout);
-
+        /*
+        setting up for appbar for the custom length show to the user instead of full length expanded
+        view.
+         */
         appBarLayout.post(new Runnable() {
             @Override
             public void run() {
@@ -39,6 +59,10 @@ public class CollapsingToolbarLayout extends AppCompatActivity {
         });
     }
 
+    /**
+     * method to show the custom length of appbar instead of showing the full length appbar layout.
+     * @param offsetPx offset for length
+     */
     private void setAppBarOffset(int offsetPx) {
         CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) appBarLayout.getLayoutParams();
         AppBarLayout.Behavior behavior = (AppBarLayout.Behavior) params.getBehavior();
